3b64236c1beeb086069f67d42b5e72ec45bc4fe7
LuisDFJ <luis.fuju@outlook.com>
2024-06-25 15:49:49 -0600

    Compatibility changes added.

--- install.sh
+++ install.sh
@@ -6,16 +6,17 @@
 #sudo chown -R minecraft: /home/minecraft/
 
 apt update
-apt install wget screen openssl -y
+apt install wget screen openssl python3-pip -y
 
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb -O libssl1.1.deb
 dpkg -i libssl1.1.deb
 rm libssl1.1.deb
 
-python -m pip install --upgrade pip
-pip install requirements.txt
-python install.py
+python3 -m pip install --upgrade pip
+pip install -r requirements.txt
+python3 install.py
 
+chmod +x ./MinecraftServer/bedrock_server
 chmod +x ./MinecraftServer/start_server.sh
 chmod +x ./MinecraftServer/stop_server.sh
 cp ./MinecraftServer/MCServer.service /etc/systemd/system/MCServer.service
@@ -24,6 +25,7 @@ cp ./MinecraftServer/MCServer.service /etc/systemd/system/MCServer.service
 
 ufw allow 19132:19133/tcp
 ufw allow 19132:19133/udp
+ufw enable
 
 systemctl enable MCServer
 systemctl start MCServer
